954e62ada58b9d08880cbb4d5bb25219f6a528d0
Sean Workman <sean.workman.96@gmail.com>
2021-06-17 13:38:36 -0400

    final

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CS517_Approx_Derivative
+This program calculates the approximate derivative of f(x) (sin(x)) using the finite difference formula, the actual derivative (cos(x)), and the absolute error between the two for x = 1 and h: 2^-1 -> 2^-30.
+
+- Run `python3 approx_derivative.py`
+
+The graph of h vs absolute error values can be found in h_vs_abs_error.png, or can be created by uncommenting `plotResults(xValues, yValues)`.
+
+The end result will be in the form:
+|  h   |     x      | Approx. f’(x) | Known f’(x) | Abs. Error |
+|:----:|:----------:|:-------------:|:-----------:|:----------:|
+|2^-01 | 1.00000000 |    0.31204800 |  0.54030231 | 0.22825430 |
+|2^-02 | 1.00000000 |    0.43005454 |  0.54030231 | 0.11024777 |
+|2^-03 | 1.00000000 |    0.48637287 |  0.54030231 | 0.05392943 |
+|2^-04 | 1.00000000 |    0.51366321 |  0.54030231 | 0.02663910 |
+The minimum value for the magnitude of the error is 1.850815717229236e-09, √eps is 1.4901161193847656e-08, the difference between them is 1.305034547661842e-08.
---
+++ approx_derivative.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import sys
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+X_VAL = 1.0
+
+# Helper to make code cleaner
+def sin(a):
+    return math.sin(a)
+
+# Helper to make code cleaner
+def cos(a):
+    return math.cos(a)
+
+# Calculates the derivative using finite difference formula
+# f'(x) = [f(x+h) - f(x)]/[h]
+def approx(h):
+    fxh = sin(X_VAL + h)
+    fx = sin(X_VAL)
+    fdx = (fxh - fx) / h
+    return fdx
+
+# Calculate the derivative using cos(x)
+def known():
+    return cos(X_VAL)
+
+# Helper funtion to calculate the absolute error
+# ap: approximate derivative
+# k: known derivative 
+def absError(ap, k):
+    return abs(ap-k)
+
+# Calculates and builds results 
+def calculate():
+    result = []
+    xValues = []
+    yValues = []
+
+    for i in range(1, 31):
+        h = 2 ** (-1 * i)
+        if i < 10:
+            hString = "2^-0{}".format(i)
+        else:
+            hString = "2^-{}".format(i)
+        # Approximate Derivative
+        ap = approx(h)
+        # Known Derivative
+        k = known()
+        # Absolute Error
+        absE = absError(ap, k)
+        xValues.append(h)
+        yValues.append(absE)
+        resDict = {
+            'h': hString,
+            'x': round(X_VAL, 9),
+            'approx': round(ap, 9),
+            'known': round(k, 9),
+            'absError': round(absE, 9)
+        }
+        result.append(resDict)
+
+    # Uncomment to plot h vs absolute error values
+    # plotResults(xValues, yValues)
+
+    minError = min(yValues)
+
+    return result
+
+# Helper function to plot h vs absolute error values
+def plotResults(xValues, yValues):
+    xpoints = np.array(xValues)
+    ypoints = np.array(yValues)
+    plt.plot(xpoints, ypoints)
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.show()
+
+# Helper function to print the results 
+def printResult(result):
+    print("|  h  | x | Approx. f'(x) | Known f'(x) | Abs. Error |")
+    print("|:---:|:-:|:-------------:|:-----------:|:----------:|")
+    for i in result:
+        print("|{}|{}|    {}|  {}| {}|".format(i['h'], i['x'], i['approx'], i['known'], i['absError']))
+
+# Is there a minimum value for the magnitude of the error? If such a value exists, how does it compare to √eps (Use the
+# Cleve Moler Algorithm to approximate eps.)
+def minErrorVEps(result):
+    errors = []
+    for i in result:
+        # Calculate and store the relative error
+        errors.append(i['absError'] / i['known'])
+
+    # Find the minimum magnitude of the error
+    minError = min(errors)
+    # Calculate the machine epsilon and square root it for √eps
+    eps = np.finfo(float).eps
+    sqrtEPS = math.sqrt(eps)
+
+    print("The minimum value for the magnitude of the error is {}, √eps is {}, the difference between them is {}.".format(minError, sqrtEPS, (abs(minError - sqrtEPS))))
+
+result = calculate()
+printResult(result)
+minErrorVEps(result)
